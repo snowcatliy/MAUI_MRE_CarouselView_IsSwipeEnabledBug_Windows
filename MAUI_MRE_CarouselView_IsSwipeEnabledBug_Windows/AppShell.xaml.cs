0d5fc00b0877c66230e21df3a3314ba6750fe671
snowcatliy <1147253786@qq.com>
2025-04-26 16:02:14 +0800

    添加项目文件。

---
+++ MAUI_MRE_CarouselView_IsSwipeEnabledBug_Windows/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_MRE_CarouselView_IsSwipeEnabledBug_Windows
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MAUI_MRE_CarouselView_IsSwipeEnabledBug_Windows/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUI_MRE_CarouselView_IsSwipeEnabledBug_Windows
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
